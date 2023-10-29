a2ae47da8dd8faebbaa8ea97790cd7e42ed904f5
Taiki Kondo <taiki@defios.jp>
2023-10-30 02:04:43 +0900

    refactor matmul_gpu

--- chap06/onnx_vc6/layer/matmul_gpu.py
+++ chap06/onnx_vc6/layer/matmul_gpu.py
@@ -8,66 +8,47 @@ class MatMul_gpu(Layer):
     def __init__(self,model,node,tensor_data,use_gpu=False,drv=None):
         super().__init__(model,node,tensor_data,use_gpu)
         self.drv=drv
-        self.copy_flg1=False
-        self.copy_flg2=False
-        self.num_qpus=8
-        self.simd_width=16
-        num_thy=2
-        num_thx=4        
-        self.input_data1 = self.tensor_data[self.input_name[0]] 
-        self.input_data2 = self.tensor_data[self.input_name[1]]
+        self.simd_width=16        
+        self.input_data1 = self.alloc_gpu_for_initializer(self.input_name[0])
+        self.input_data2 = self.alloc_gpu_for_initializer(self.input_name[1])
         self.output_data = self.tensor_data[self.output_name]
         data1_shape=list(self.input_data1.shape)
         data2_shape=list(self.input_data2.shape)
         output_shape=list(self.output_data.shape)
         self.p,self.q = data1_shape[-2:]
         self.q,self.r = data2_shape[-2:]
-        self.p_mod = self.p % (num_thy*16)
-        self.r_mod = self.r % (num_thx*32)
-        hblock=self.p/num_thy
-        wblock=self.r/num_thx
-        j_idx=wblock/32
-        i_idx=hblock/16
+
+        if self.p < 32 or self.r < 128:
+            num_thx = 1
+            num_thy = 1
+        else:
+            num_thx=4
+            num_thy=2
+            
+        assert self.p >= 16 and self.r >= 32, "The matrix size must satisfy p <= 16 and r <= 32."
+        
+        self.num_qpus = num_thx*num_thy
+
+
+        #1スレッドが処理する範囲
+        hblock=int(math.ceil(self.p/num_thy))
+        wblock=int(math.ceil(self.r/num_thx))
+
+        #スレッド分割の端数
+        #GPUカーネル内で以下の処理を行う
+        # if thread_id.x == 3:
+        #     WBLOCK = WBLOCK - frac_w
+        # if thread_id.y == 1:
+        #     HBLOCK = HBLOCK - frac_h
+        frac_w = (wblock * num_thx - self.r) * 4
+        frac_h = (hblock * num_thy - self.p)
+
+        #ループ回数
+        j_idx=int(math.ceil(self.r / (32.0 * num_thx)))
+        i_idx=int(math.ceil(self.p / (16.0 * num_thy)))
         k_idx=self.q
-        if self.p_mod > 0:
-            new_p = math.ceil(self.p / (num_thy*16)) * (num_thy*16)
-            data1_shape[-2] = int(new_p)
-            output_shape[-2] = int(new_p)
-            self.input_data1 = self.drv.alloc(data1_shape,dtype=self.input_data1.dtype)
-            if self.input_name[0] in self.init_names:
-                self.input_data1[...,:self.p,:self.q]=self.tensor_data[self.input_name[0]]
-                self.tensor_data[self.input_name[0]]=self.input_data1
-            else:
-                self.copy_flg1=True
-            hblock=new_p/num_thy
-            i_idx=hblock/16
-        else:        
-            if self.input_name[0] in self.init_names:
-                cpu_w = self.tensor_data[self.input_name[0]]
-                self.tensor_data[self.input_name[0]] = self.drv.alloc(cpu_w.shape,cpu_w.dtype)
-                self.tensor_data[self.input_name[0]][:]=cpu_w
-                self.input_data1 = self.tensor_data[self.input_name[0]]
-                
-        if self.r_mod > 0:
-            new_r = math.ceil(self.r / (num_thx*32)) * (num_thx*32)
-            data2_shape[-1] = int(new_r)
-            output_shape[-1] = int(new_r)
-            self.input_data2 = self.drv.alloc(data2_shape,dtype=self.input_data2.dtype)      
-            if self.input_name[1] in self.init_names:
-                self.input_data2[...,:self.q,:self.r]=self.tensor_data[self.input_name[1]]
-                self.tensor_data[self.input_name[1]]=self.input_data2
-            else:
-                self.copy_flg2=True
-            wblock=new_r/num_thx
-            j_idx=wblock/32
-        else:        
-            if self.input_name[1] in self.init_names:
-                cpu_w = self.tensor_data[self.input_name[1]]
-                self.tensor_data[self.input_name[1]] = self.drv.alloc(cpu_w.shape,cpu_w.dtype)
-                self.tensor_data[self.input_name[1]][:]=cpu_w
-                self.input_data2 = self.tensor_data[self.input_name[1]]
-        if self.r_mod > 0 or self.p_mod >0:
-            self.output_data = self.drv.alloc(output_shape,dtype=self.output_data.dtype)
+        wblock = int(wblock * 4) #float = 4bytes
+
         self.output_data[:]=0
         self.unif = self.drv.alloc(16, dtype='uint32')
         self.unif[0] = self.input_data1.address
@@ -76,18 +57,38 @@ class MatMul_gpu(Layer):
         self.unif[3] = self.input_data2.strides[-2]
         self.unif[4] = self.output_data.address
         self.unif[5] = hblock
-        self.unif[6] = wblock*4 #float
+        self.unif[6] = wblock
         self.unif[7] = i_idx
         self.unif[8] = j_idx
         self.unif[9] = k_idx
+        self.unif[10] = frac_w
+        self.unif[11] = frac_h        
         self.code = self.drv.program(kernel, num_qpus=self.num_qpus)        
             
     def run(self):
-        if self.copy_flg1:
-            self.input_data1[...,:self.p,:self.q]=self.tensor_data[self.input_name[0]]
-        if self.copy_flg2:
-            self.input_data2[...,:self.q,:self.r]=self.tensor_data[self.input_name[1]]        
+        # if self.copy_flg1:
+        #     self.input_data1[...,:self.p,:self.q]=self.tensor_data[self.input_name[0]]
+        # if self.copy_flg2:
+        #     self.input_data2[...,:self.q,:self.r]=self.tensor_data[self.input_name[1]]        
 
+        # inp1_shape = list(self.input_data1.shape)
+        # inp2_shape = list(self.input_data2.shape)
+        # if len(inp1_shape) <3:
+        #     inp1_shape.insert(0,1)
+        # if len(inp2_shape) <3:
+        #     inp2_shape.insert(0,1)            
+        # inp1_range = range(inp1_shape[-3])
+        # inp2_range = range(inp2_shape[-3])
+        # max_len = max(len(inp1_range),len(inp2_range))
+        # if max_len > len(inp1_range):
+        #     inp1_range=[0]*max_len
+        # if max_len > len(inp2_range):
+        #     inp2_range=[0]*max_len
+        # offset1=len(inp1_shape)*[0]
+        # offset2=len(inp2_shape)*[0]
+        # offset3=max(len(inp2_shape),len(inp1_shape))*[0]
+        # print(self.input_name[0])
+        #3次元対応
         inp1_shape = list(self.input_data1.shape)
         inp2_shape = list(self.input_data2.shape)
         if len(inp1_shape) <3:
@@ -96,14 +97,9 @@ class MatMul_gpu(Layer):
             inp2_shape.insert(0,1)            
         inp1_range = range(inp1_shape[-3])
         inp2_range = range(inp2_shape[-3])
-        max_len = max(len(inp1_range),len(inp2_range))
-        if max_len > len(inp1_range):
-            inp1_range=[0]*max_len
-        if max_len > len(inp2_range):
-            inp2_range=[0]*max_len
         offset1=len(inp1_shape)*[0]
         offset2=len(inp2_shape)*[0]
-        offset3=max(len(inp2_shape),len(inp1_shape))*[0]                
+        offset3=max(len(inp2_shape),len(inp1_shape))*[0]             
         for i,j in zip(inp1_range,inp2_range):
             offset1[-3]=i
             offset2[-3]=j
@@ -115,14 +111,13 @@ class MatMul_gpu(Layer):
             if len(self.output_data.shape) > 2:                
                 self.unif[4] = self.output_data.addresses()[tuple(offset3)]
             self.drv.execute(self.code, self.unif.addresses()[0], thread=self.num_qpus)
-            
         else:
             self.drv.execute(self.code, self.unif.addresses()[0], thread=self.num_qpus)        
-
-        gpu = self.output_data
+            
+        # gpu = self.output_data
         # cpu = self.input_data1@self.input_data2
         # print('maximum relative error : {:.4e}'.format(float(np.max(np.abs(cpu-gpu)/np.maximum(np.abs(cpu),1e-10)))))        
-        self.tensor_data[self.output_name][:] = gpu[...,:self.p,:self.r]
+        # self.tensor_data[self.output_name][:] = gpu[...,:self.p,:self.r]
 
 
 
@@ -139,12 +134,12 @@ def kernel(asm, num_qpus):
     I_SIZE=7
     J_SIZE=8
     K_SIZE=9
-    I_IDX=10
-    J_IDX=11
-    k_IDX=12
-
+    FRAC_W=10
+    FRAC_H=11
+    
+    
     eidx(r0).mov(r2, 0)
-    for idx in [A_ADDR, A_STR, B_ADDR, B_STR, C_ADDR, HBLOCK, WBLOCK, I_SIZE, J_SIZE, K_SIZE]:
+    for idx in [A_ADDR, A_STR, B_ADDR, B_STR, C_ADDR, HBLOCK, WBLOCK, I_SIZE, J_SIZE, K_SIZE, FRAC_W, FRAC_H]:
         nop(sig=ldunifrf(r5))
         sub(null, r0, idx, cond='pushz')
         mov(r2, r5, cond='ifa')
@@ -168,18 +163,31 @@ def kernel(asm, num_qpus):
     eidx(r4)
     sub(r1,r0,4,cond='pushn')
     mov(r1,r0,cond='ifa')
-    rotate(broadcast,r2,-WBLOCK)
-    umul24(r1,r5,r1)
+    rotate(broadcast,r2,-WBLOCK)    
+    umul24(r3,r5,r1)
+
+    #端数処理
+    rotate(broadcast,r2,-FRAC_W)
+    sub(null,r1,3,cond='pushz')
+    sub(r3,r3,r5, cond='ifa')
+    mov(r1,r3)
+    
     sub(null, r4, B_ADDR, cond='pushz')
     add(r2, r2, r1, cond='ifa')    
 
-    
+
+
+        
     #numqpu%4
     #A set
     
     shr(r3,r0,2)
     rotate(broadcast,r2,-HBLOCK)
-    umul24(r3,r5,r3)
+    mov(r0,r5)
+    rotate(broadcast,r2,-FRAC_H)
+    sub(r0,r0,r5)
+    
+    umul24(r3,r0,r3)
     rotate(broadcast,r2,-B_STR) #for C
     umul24(r0,r3,r5)
     
@@ -191,7 +199,8 @@ def kernel(asm, num_qpus):
     #C set
     add(r1,r1,r0)
     sub(null, r4, C_ADDR, cond='pushz')
-    add(r2, r2, r1, cond='ifa')        
+    add(r2, r2, r1, cond='ifa')
+    
     iidx=rf50
     jidx=rf51
     kidx=rf52
@@ -218,8 +227,26 @@ def kernel(asm, num_qpus):
         # mov(r0,1)
         # shl(r0,r0,4)
         umul24(r0,ldi16,iidx)
+
+        #端数処理 
+        # if HBLOCK - i * 16 < 0:
+        #     i - (16 + (HBLOCK - i*16))        
+        add(r1,r0,ldi16)
+        rotate(broadcast,r2,-HBLOCK)        
+        sub(r1,r5,r1,cond = 'pushn')
+        b(R.fraction_i_end,cond='anyna')
+        mov(r1,0) #nop
+        eidx(a_cur) #nop
+        rotate(broadcast,r2,-A_STR) #nop
+
+        add(r0,r0,r1)
         eidx(a_cur)
         rotate(broadcast,r2,-A_STR)
+        
+        L.fraction_i_end
+        
+        # eidx(a_cur)
+        # rotate(broadcast,r2,-A_STR) 
         umul24(a_cur,a_cur,r5)
         umul24(r0,r5,r0)
         add(a_cur,a_cur,r0)
@@ -231,10 +258,27 @@ def kernel(asm, num_qpus):
             #1 : 32 x 4(float) x jidx
             umul24(r0,ldi128,jidx)
 
-
-            #2 : eidx x 4 + B_ADDR
+            # if WBLOCK - j * 32 * 4(bytes) < 0:
+            #     r0 - (128 + (WBLOCK - j * 128)
+            add(r3,r0,ldi128)
+            rotate(broadcast,r2,-WBLOCK)
+            sub(r3,r5,r3,cond = 'pushn')
+            b(R.fraction_j_end,cond='anyna')
             mov(kidx,0)
             eidx(b_cur)
+            mov(rf49,0)
+
+        
+            add(r0,r0,r3)
+            mov(rf49,r3)
+            mov(kidx,0)
+            eidx(b_cur)
+
+            L.fraction_j_end
+            
+            #2 : eidx x 4 + B_ADDR
+            # mov(kidx,0)
+            # eidx(b_cur)
             shl(b_cur,b_cur,2)
             rotate(broadcast,r2,-B_ADDR)
             add(b_cur,b_cur,r5)
@@ -278,15 +322,20 @@ def kernel(asm, num_qpus):
             eidx(c_cur)
             rotate(broadcast,r2,-B_STR)
             umul24(c_cur,c_cur,r5)
+            umul24(r1,r1,r5) # 端数処理
+            
             umul24(r0,r5,r0)
             add(c_cur,c_cur,r0)
             
             # mov(r0,1)
             # shl(r0,r0,7)
-            umul24(r0,ldi128,jidx)            
+            umul24(r0,ldi128,jidx)
+            add(r0,r0,rf49)
             rotate(broadcast,r2,-C_ADDR)
             add(c_cur,c_cur,r5)
             add(c_cur,c_cur,r0)
+            add(c_cur,c_cur,r1) #端数処理
+
             for li in range(32):
                 # fmul(r3,r3,2.0)
                 # mov(tmud,r3)
